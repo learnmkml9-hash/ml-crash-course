ada1ff1ae6a87a98cdf4792de1858df86c564237
learnmkml <learn.mk.ml.9@gmail.com>
2026-06-21 22:27:11 -0500

    Add Python 3.12 environment setup

---
+++ activate_ml_env_312.sh
@@ -0,0 +1,3 @@
+source .venv312/bin/activate
+export SSL_CERT_FILE=$(python -c "import certifi; print(certifi.where())")
+export REQUESTS_CA_BUNDLE=$(python -c "import certifi; print(certifi.where())")
